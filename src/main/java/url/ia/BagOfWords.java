d2a5c307649d801c02f75237f44433e3d263751e
MarlieRamirez <marlie.lucia@hotmail.com>
2024-05-26 12:16:16 -0600

    Normalizar datos

--- src/main/java/url/ia/BagOfWords.java
+++ src/main/java/url/ia/BagOfWords.java
@@ -3,8 +3,10 @@ package url.ia;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.regex.Pattern;
 
 public class BagOfWords {
     private static BagOfWords instance;
@@ -21,13 +23,15 @@ public class BagOfWords {
 
     //Agregar con un key existente o uno nuevo
     public void add(String phrase, String key){
+        //Eliminar caracteres especiales, solo espacios, letras y sus acentos
+        phrase = phrase.replaceAll("[^a-záéíóúüñ ]", "");
+
         if (map.containsKey(key)){
             map.put(key, Stream.concat(map.get(key).stream(), Stream.of(phrase)).collect(Collectors.toList()));
+
         }else{
             map.put(key,List.of(phrase));
-            //System.out.println("NEW: "+key + " - " + map.get(key));
         }
-        //System.out.println(map.toString());
     }
 
 
